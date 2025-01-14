51d7f2894d7ce41be3a3dc7ff693e72f843b34a7
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-14 14:32:30 +0700

    feat: add admin upload endpoint

--- src/main/java/com/charitan/profile/asset/AssetController.kt
+++ src/main/java/com/charitan/profile/asset/AssetController.kt
@@ -5,6 +5,7 @@ import jakarta.annotation.security.RolesAllowed
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,9 +22,21 @@ class AssetController(
         @RequestBody fileName: String,
     ): ResponseEntity<String> =
         ResponseEntity.ok(
-            // add random number to invalidates cache (if exists)
             assetService.signedUploadUrl(
                 "${userDetails.userId}/$fileName",
+            ),
+        )
+
+    @RolesAllowed("ADMIN")
+    @PostMapping("/upload/{id}")
+    fun getUploadSignedUrl(
+        @AuthenticationPrincipal userDetails: CustomUserDetails,
+        @PathVariable id: String,
+        @RequestBody fileName: String,
+    ): ResponseEntity<String> =
+        ResponseEntity.ok(
+            assetService.signedUploadUrl(
+                "$id/$fileName",
             ),
         )
 }
